ef370ed88977409027e18d90ab104529dfc95c6a
Viktor Moisseyenko <vitek.mo@gmail.com>
2019-10-13 18:06:53 +0600

    Step 3

---
+++ src/cli.php
@@ -0,0 +1,14 @@
+<?php
+namespace BrainGames\Cli;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use function \cli\line;
+use function \cli\prompt;
+
+function run()
+{
+	line('Welcome to the Brain Game!');
+	$name = prompt('May i have your name?');
+	line("Hello, %s", $name);
+}
